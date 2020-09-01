62c6d6ca7bd9f8ac26d0c0962aa7879faee27429
oitm-coronacryot-dev <oitm.coronacryot.dev@protonmail.com>
2020-09-01 23:08:36 +0200

    CoronaCrypt

---
+++ CoronaCrypt.js
@@ -0,0 +1,112 @@
+// CoronaCrypt blockchain
+
+// CoronaCrypt block
+class CoronaCryptBlock {
+
+    constructor(index, timestamp, data, precedingHash=" ", hash=" ") {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.precedingHash = precedingHash;
+        this.hash = hash;
+    }
+
+// computing the hash
+// TUDO: use cryptography strong hash function !!!
+    Hash() {
+        var block_sting = JSON.stringify({index: this.index, timestamp: this.timestamp, data: this.data, precedingHash:this.precedingHash});
+        var hash = 0;
+        for (var i = 0; i < block_sting.length; i++) {
+            var i_char = block_sting.charAt(i);
+            var i_byte = i_char.charCodeAt(0);
+            hash = hash + i_byte;
+            if (hash > 65535) {
+                hash = hash - 65535;
+            }
+        }
+        return hash.toString();
+    }
+
+    // compute and save hash
+    computeHash() {
+        this.hash = this.Hash();
+    }
+}
+
+// CoronaCrypt chain
+class CoronaCryptBlockchain {
+
+    constructor() {
+        this.blockchain = [];
+    }
+
+    // initialize a new chain with genesis block
+    initChane() {
+        this.blockchain = [];
+        let GenesisBlock = new  CoronaCryptBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+        GenesisBlock.computeHash();
+        this.blockchain.push(GenesisBlock);
+    }
+
+    // get lastest block
+    obtainLatestBlock() {
+        return this.blockchain[this.blockchain.length - 1];
+    }
+
+    // add new block, fill preceding hash and calculate hash
+    addNewBlock(newBlock) {
+        newBlock.precedingHash = this.obtainLatestBlock().hash;
+        newBlock.computeHash();
+        this.blockchain.push(newBlock);
+    }
+
+    // validate chain
+    checkChainValidity() {
+        for (let i = 1; i < this.blockchain.length; i++) {
+            const currentBlock = this.blockchain[i];
+            const precedingBlock = this.blockchain[i - 1];
+            
+            // check the hash of the current block
+            if (currentBlock.hash !== currentBlock.Hash()) {
+                console.log("current block hash do not matches with the calculated hash!");
+                console.log("hash in current block: "+currentBlock.hash);
+                console.log("calculated hash: "+currentBlock.Hash());
+                return false;
+            }
+
+            // check the hash of the preceding block
+            if (currentBlock.precedingHash !== precedingBlock.Hash()) {
+                console.log("preceding block hash do not matches with the calculated hash!");
+                console.log("precedingHash in current block: "+currentBlock.precedingHash);
+                console.log("calculated precedingHash: "+precedingBlock.Hash());
+                return false;
+            }
+        }
+        
+        console.log("chain is valid!");
+        return true;
+    }
+
+    // serialize chain to JSON
+    serialize(){
+        return JSON.stringify(this.blockchain, null, 2);
+    }
+
+    // load a chain from JSON
+    deserialize(inputstring){
+        this.blockchain = [];
+        var blocks = JSON.parse(inputstring);
+        for (let i = 0; i < blocks.length; i++) {
+            let newblock = new  CoronaCryptBlock(
+                blocks[i].index,
+                blocks[i].timestamp,
+                blocks[i].data,
+                blocks[i].precedingHash,
+                blocks[i].hash,
+                );
+            console.log("load block:");
+            console.log(JSON.stringify(newblock, null, 2));
+            this.blockchain.push(newblock);
+        }
+    }
+}
---
+++ tools.js
@@ -0,0 +1,41 @@
+function ValidateChain() {
+    // deserialise input chain
+    var inputChainString = document.getElementById("validate_input").value;
+    let chain = new CoronaCryptBlockchain();
+    chain.deserialize(inputChainString);
+
+    // validate
+    if (chain.checkChainValidity()) {
+        document.getElementById("validate_output").value = "Valid";
+    } else{
+        document.getElementById("validate_output").value = "Invalid";
+    }
+}
+
+function NewChain() {
+    // init chain
+    let chain = new CoronaCryptBlockchain();
+    chain.initChane();
+
+    // serialise output chain
+    document.getElementById("newchain_output").value = chain.serialize();
+}
+
+function AddBlockToChain(){
+    // deserialise input chain
+    var inputChainString = document.getElementById("addblock_input").value;
+    let chain = new CoronaCryptBlockchain();
+    chain.deserialize(inputChainString);
+
+    // new block parameters
+    var inputIndex = document.getElementById("addblock_index").value;
+    var inputTimestamp = document.getElementById("addblock_timestamp").value;
+    var inputData = document.getElementById("addblock_data").value;
+    
+    // add new block
+    let newBlock = new CoronaCryptBlock(inputIndex, inputTimestamp, inputData);
+    chain.addNewBlock(newBlock);
+
+    // serialise output chain
+    document.getElementById("addblock_output").value = chain.serialize();
+}
